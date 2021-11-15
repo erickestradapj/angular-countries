662bcafcc47bbccb5f273ae33f8154bd5a970093
Erick Estrada <erickestradapj@gmail.com>
2021-11-15 15:09:32 -0500

    view country completed

--- src/app/country/pages/view-country/view-country.component.ts
+++ src/app/country/pages/view-country/view-country.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-view-country',
@@ -8,18 +10,20 @@ import { CountryService } from '../../services/country.service';
   styles: [],
 })
 export class ViewCountryComponent implements OnInit {
+  country!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private countryService: CountryService
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ id }) => {
-      console.log(id);
-
-      this.countryService.getCountryByCode(id).subscribe((country) => {
-        console.log(country);
-      });
-    });
+    // TODO: Optimize later
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.countryService.getCountryByCode(id)),
+        tap(console.log)
+      )
+      .subscribe((country) => (this.country = country[0]));
   }
 }
